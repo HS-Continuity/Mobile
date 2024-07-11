4517a0e0da2b29b4e20198d910d68a4462ca0415
정승수 <victory-soo@naver.com>
2024-07-11 14:32:55 +0900

    KAN-202 refactor: 스크롤바 제거

--- src/components/Layouts/MobileMain.jsx
+++ src/components/Layouts/MobileMain.jsx
@@ -3,9 +3,9 @@ import style from "./Main.module.css";
 
 const Main = () => {
   return (
-    <div className='relative mx-auto min-h-screen w-full bg-white sm:max-w-full md:max-w-full lg:max-w-[500px] xl:max-w-[500px]'>
+    <div className='main-container relative mx-auto min-h-screen w-full bg-white'>
       <div className='flex h-screen flex-col'>
-        <main className={`${style.noScrollbar} flex-grow overflow-y-auto pb-16`}>
+        <main className='noScrollbar flex-grow overflow-y-auto'>
           <Outlet />
         </main>
       </div>

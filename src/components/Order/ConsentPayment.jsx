bfed11d17b444c9a368854d8726cc3f136e04640
정승수 <victory-soo@naver.com>
2024-07-15 15:24:17 +0900

    KAN-203 refactor: 결제하기 버튼 클릭시 이동
    - 결제하기 버튼 클릭시 /order-success 링크 이동 수정

--- src/components/Order/ConsentPayment.jsx
+++ src/components/Order/ConsentPayment.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const ConsentPayment = ({ consentPayment, finalPrice }) => {
+  const navigate = useNavigate();
+
+  const handleOrderSuccess = e => {
+    e.preventDefault();
+    navigate("/order-success");
+  };
+
   return (
     <>
       <button
         className={`bg-[#00835F] p-4 text-lg font-bold text-white ${
           consentPayment ? "" : "cursor-not-allowed bg-gray-400"
         }`}
+        onClick={handleOrderSuccess}
         disabled={!consentPayment}>
         {finalPrice.toLocaleString()}원 결제하기
       </button>

544882f46cd4283800fe9a6ac942dbc43da86501
정승수 <victory-soo@naver.com>
2024-07-18 19:37:47 +0900

    KAN-353 hotfix: API 충돌 해결

--- src/apis/Product.js
+++ src/apis/Product.js
@@ -21,6 +21,18 @@ const fetchProducts = async ({ pageParam = 0 }) => {
   }
 };
 
+// [GET] 상품 상세 이미지 조회
+export const fetchProductDetailImage = async productId => {
+  try {
+    const response = await axios.get(`${DB_URL}/product-image/${productId}`);
+    // console.log(response.data.result);
+    return response.data.result;
+  } catch (error) {
+    console.error("Error 상품 상세 이미지 조회:", error);
+    throw error;
+  }
+};
+
 // [GET] 상품 전체 무한 스크롤 조회
 export const useProductsQuery = () => {
   return useInfiniteQuery({
@@ -33,6 +45,68 @@ export const useProductsQuery = () => {
       return lastPage.number + 1;
     },
   });
+};
+
+// [GET] 상품 내용 상세 조회
+export const fetchProductDetail = async productId => {
+  try {
+    const response = await axios.get(`${DB_URL}/shopping/product/${productId}`);
+    // console.log(response.data.result);
+    return response.data.result;
+  } catch (error) {
+    console.error("Error 상품 내용 상세 조회:", error);
+    throw error;
+  }
+};
+
+// [GET] 상품별 리뷰 조회
+export const fetchLatestProductReviews = async productId => {
+  try {
+    const response = await axios.get(`${DB_URL}/product-review/${productId}`, {
+      params: {
+        pageSize: 3,
+      },
+    });
+    return response.data.result.content;
+  } catch (error) {
+    console.error("Error 상품 최신 리뷰 조회:", error);
+    throw error;
+  }
+};
+
+// [GET] 상품 리뷰 전체 조회
+export const fetchProductReviews = async (productId, startPage = 0, sortOption = "latest") => {
+  try {
+    let sort, direction;
+    switch (sortOption) {
+      case "lowRating":
+        sort = "reviewScore";
+        direction = "asc";
+        break;
+      case "highRating":
+        sort = "reviewScore";
+        direction = "desc";
+        break;
+      case "latest":
+      default:
+        sort = "createDate";
+        direction = "desc";
+        break;
+    }
+
+    const response = await axios.get(`${DB_URL}/product-review/${productId}`, {
+      params: {
+        startPage,
+        pageSize: 10,
+        sort,
+        direction,
+      },
+    });
+    return response.data.result;
+  } catch (error) {
+    console.error("Error fetching product reviews:", error);
+    throw error;
+  }
 };
 
 // [GET] 상품 검색
@@ -67,15 +141,4 @@ export const useSearchProductsQuery = keyword => {
     },
     enabled: !!keyword,
   });
-};
-// [GET] 상품 내용 상세 조회
-export const fetchProductDetail = async productId => {
-  try {
-    const response = await axios.get(`${DB_URL}/shopping/product/${productId}`);
-    // console.log(response.data.result);
-    return response.data.result;
-  } catch (error) {
-    console.error("Error 상품 내용 상세 조회:", error);
-    throw error;
-  }
 };

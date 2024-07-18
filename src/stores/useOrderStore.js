62b817229f849263b1ad35eaed9df92c445b60fa
정승수 <victory-soo@naver.com>
2024-07-18 18:16:52 +0900

    KAN-345 fix: 사용 안하는 코드 주석 처리
    - API 변경하면서 사용하지 않는 코드 우선 주석 처리(추후 삭제 예정)

--- src/stores/useOrderStore.js
+++ src/stores/useOrderStore.js
@@ -1,8 +1,26 @@
 import { create } from "zustand";
 
+// const useOrderStore = create(set => ({
+//   orders: [],
+//   setOrders: orders => set({ orders }),
+// }));
+
 const useOrderStore = create(set => ({
-  orders: [],
-  setOrders: orders => set({ orders }),
+  // orderItems: [],
+  // totalProductPrice: 0,
+  // totalDeliveryFee: 0,
+  // setOrderData: (items, productPrice, deliveryFee) =>
+  //   set({
+  //     orderItems: items,
+  //     totalProductPrice: productPrice,
+  //     totalDeliveryFee: deliveryFee,
+  //   }),
+  // clearOrderData: () =>
+  //   set({
+  //     orderItems: [],
+  //     totalProductPrice: 0,
+  //     totalDeliveryFee: 0,
+  //   }),
 }));
 
 export default useOrderStore;

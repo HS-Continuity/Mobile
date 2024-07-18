9a58c0830677792ab8a2c60e36b1258fbb67a656
정승수 <victory-soo@naver.com>
2024-07-18 18:24:37 +0900

    KAN-345 fix: 반응형 클래스명 수정
    - 반응형 클래스명 전역 변수 처리한 것으로 대체

--- src/components/Layouts/Main.jsx
+++ src/components/Layouts/Main.jsx
@@ -9,14 +9,13 @@ const Main = () => {
   const mainRef = useRef(null);
 
   useEffect(() => {
-    // 라우트 변경 시 스크롤을 최상단으로 이동
     if (mainRef.current) {
       mainRef.current.scrollTop = 0;
     }
   }, [location.pathname]);
 
   return (
-    <div className='relative mx-auto min-h-screen w-full bg-white sm:max-w-full md:max-w-full lg:max-w-[500px] xl:max-w-[500px]'>
+    <div className='main-container relative mx-auto min-h-screen w-full bg-white'>
       <div className='flex h-screen flex-col'>
         <TopHeader />
         <main ref={mainRef} className={`${style.noScrollbar} flex-grow overflow-y-auto p-4 pb-16`}>

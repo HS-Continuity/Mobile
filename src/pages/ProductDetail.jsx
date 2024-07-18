f7fc318fe78318a4e60872fbc8e486fb9197be71
정승수 <victory-soo@naver.com>
2024-07-18 18:59:03 +0900

    KAN-353 refactor: 변수명 수정

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,17 +2,19 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FaChevronDown, FaHeart, FaMinus, FaPlus, FaShare, FaStar } from "react-icons/fa";
 import { CiShop, CiShoppingCart } from "react-icons/ci";
-import { fetchProduct, getLastCartItemId, postToCart } from "../apis";
+// import { getLastCartItemId, postToCart } from "../apis";
 import PriceBox from "../components/Product/PriceBox";
 import DetailImage from "../components/Product/DetailImage";
-import { useProductReviewStore } from "../stores/useProductReviewStore";
+// import { useProductReviewStore } from "../stores/useProductReviewStore";
 import ReviewList from "../components/Product/ReviewList";
 import { useState } from "react";
 import { BsLightningChargeFill } from "react-icons/bs";
+import { fetchProductDetail, fetchProductDetailImage } from "../apis/Product";
+import { postCartItem } from "../apis/Cart";
 
 const ProductDetail = () => {
+  const memberId = import.meta.env.VITE_MEMBER_ID;
   const { productId } = useParams();
-  const { averageRating } = useProductReviewStore();
   const [isAddCartOpen, setIsAddCartOpen] = useState(false);
   const [quantity, setQuantity] = useState(1);
   const [purchaseType, setPurchaseType] = useState("regular");
@@ -25,11 +27,20 @@ const ProductDetail = () => {
     isError,
   } = useQuery({
     queryKey: ["product", productId],
-    queryFn: () => fetchProduct(productId),
+    queryFn: () => fetchProductDetail(productId),
+  });
+
+  const {
+    data: images,
+    isImageLoading,
+    isImageError,
+  } = useQuery({
+    queryKey: ["images", productId],
+    queryFn: () => fetchProductDetailImage(productId),
   });
 
   const addToCartMutation = useMutation({
-    mutationFn: postToCart,
+    mutationFn: postCartItem,
     onSuccess: () => {
       queryClient.invalidateQueries("cart");
       navigate("/cart");
@@ -39,31 +50,16 @@ const ProductDetail = () => {
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading product</div>;
 
-  const baseDiscountRate = product.base_discount_rate || 0;
-  const regularDiscountRate = product.regular_discount_rate || 0;
-  const originalPrice = product.product_price;
-  const baseDiscountedPrice = originalPrice * (1 - baseDiscountRate / 100);
-  const regularDiscountedPrice = originalPrice * (1 - regularDiscountRate / 100);
-
   const handleIncrease = () => setQuantity(quantity + 1);
   const handleDecrease = () => quantity > 1 && setQuantity(quantity - 1);
 
   const handleAddToCart = async () => {
-    const lastId = await getLastCartItemId();
-    const newId = String(Number(lastId) + 1);
-
     const cartItem = {
-      id: newId,
-      product_id: Number(productId),
-      member_id: 1,
-      cart_type_id: purchaseType,
-      quantity,
-      name: product.product_name,
-      price: purchaseType === "regular" ? baseDiscountedPrice : regularDiscountedPrice,
-      originalPrice: originalPrice,
-      image: "https://www.lrt.lt/img/2022/02/09/1191080-981331-1287x836.jpg",
+      productId: Number(productId),
+      memberId: memberId,
+      cartTypeId: purchaseType === "regular" ? 1 : 2,
+      quantity: quantity,
     };
-
     addToCartMutation.mutate(cartItem);
   };
 
@@ -71,23 +67,23 @@ const ProductDetail = () => {
     <>
       <div className='container mx-auto p-4 pb-24'>
         <img
-          src={product.product_image}
-          alt={product.product_name}
+          src={product.productImage}
+          alt={product.productName}
           className='mb-4 h-64 w-full rounded-lg object-cover'
         />
         <div className='mb-4 flex items-center justify-between'>
-          <h1 className='text-2xl font-bold'>{product.product_name}</h1>
-          <Link to={`/shop/${product.customer_id}`}>
+          <h1 className='text-2xl font-bold'>{product.productName}</h1>
+          <Link to={`/shop/${product.customerId}`}>
             <div className='group flex'>
               <CiShop className='mt-1 text-xl' />
-              <h3 className='ml-1 group-hover:underline'>{product.customer_id}</h3>
+              <h3 className='ml-1 group-hover:underline'>{product.customerId}</h3>
             </div>
           </Link>
 
           <div className='flex items-center'>
             <div className='mr-4 flex items-center'>
               <FaStar className='mr-1 text-yellow-400' />
-              <span>{averageRating}</span>
+              <span>{product.averageScore}</span>
             </div>
             <div className='flex space-x-2'>
               <button className='btn btn-circle btn-sm'>
@@ -99,37 +95,38 @@ const ProductDetail = () => {
             </div>
           </div>
         </div>
-        <p className='mb-4 text-gray-600'>{product.product_description}</p>
+        <p className='mb-4 text-gray-600'>{product.productDescription}</p>
         <div className='mb-4 space-y-2'>
           <PriceBox
-            originalPrice={originalPrice}
-            discountRate={baseDiscountRate}
-            finalPrice={baseDiscountedPrice}
+            originalPrice={product.productPrice}
+            discountRate={product.baseDiscountRate}
+            finalPrice={product.calculatedBasePrice}
             isRegular={false}
           />
-          {!product.is_regular_sale && (
+          {/* 정기 배송 상품 할인 컬럼명 수정해야함 */}
+          {product.isRegularSale == "ACTIVE" && (
             <PriceBox
-              originalPrice={originalPrice}
-              discountRate={regularDiscountRate}
-              finalPrice={regularDiscountedPrice}
+              originalPrice={product.productPrice}
+              discountRate={product.regularDiscountRate}
+              finalPrice={product.calculatedRegularPrice}
               isRegular={true}
             />
           )}
         </div>
         <div className='mt-4 rounded-lg bg-gray-100 p-4'>
           <h2 className='mb-2 font-bold'>상품 정보</h2>
-          <p>원산지: {product.product_origin}</p>
-          <p>판매자: {product.customer_id}</p>
-          {!product.is_regular_sale && <p className='text-green-600'>정기 배송 가능</p>}
+          <p>원산지: {product.origin}</p>
+          <p>판매자: {product.customerId}</p>
+          {product.isRegularSale == "ACTIVE" && <p className='text-green-600'>정기 배송 가능</p>}
         </div>
-        <DetailImage imageUrl={product.product_image} />
-        <ReviewList productId={productId} product_name={product.product_name} />
+        <DetailImage imageUrl={images} />
+        <ReviewList productId={productId} product_name={product.productName} />
       </div>
 
       {isAddCartOpen ? (
         <div className='main-container fixed bottom-[120px] -ml-4 w-full rounded-t-lg bg-white p-4 pb-0 shadow'>
           <div className='mb-2 flex items-center justify-between'>
-            <span className='font-bold'>{product.product_name}</span>
+            <span className='font-bold'>{product.productName}</span>
             <div className='flex items-center space-x-2'>
               <span>수량</span>
               <div className='flex items-center rounded-md border'>
@@ -152,12 +149,12 @@ const ProductDetail = () => {
           </div>
           <div className='flex flex-col items-center justify-center space-y-2'>
             {[
-              { price: baseDiscountedPrice, label: "단건구매", type: "regular" },
-              ...(product.is_regular_sale
+              { price: product.calculatedBasePrice, label: "단건구매", type: "regular" },
+              ...(product.isRegularSale == "INACTIVE"
                 ? []
                 : [
                     {
-                      price: regularDiscountedPrice,
+                      price: product.calculatedRegularPrice,
                       label: "정기배송",
                       type: "subscription",
                       color: "text-red-500",
@@ -165,7 +162,7 @@ const ProductDetail = () => {
                   ]),
             ].map(({ price, label, type, color = "" }, index) => {
               const maxLengthLabel = Math.max(
-                ...[baseDiscountedPrice, regularDiscountedPrice].map(
+                ...[product.calculatedBasePrice, product.calculatedRegularPrice].map(
                   p => p.toLocaleString().length + label.length
                 )
               );
@@ -190,16 +187,20 @@ const ProductDetail = () => {
               );
             })}
 
-            {!product.is_regular_sale && (
-              <div className='flex items-center rounded bg-blue-100 p-2 text-sm text-blue-600'>
-                <BsLightningChargeFill className='mr-1' />
-                <span>
-                  정기 배송 시 매달{"\u00A0"}
-                  {((baseDiscountedPrice - regularDiscountedPrice) * quantity).toLocaleString()}원
-                  할인!
-                </span>
-              </div>
-            )}
+            {product.isRegularSale == "ACTIVE" &&
+              product.baseDiscountedPrice - product.calculatedRegularPrice > 0 && (
+                <div className='flex items-center rounded bg-blue-100 p-2 text-sm text-blue-600'>
+                  <BsLightningChargeFill className='mr-1' />
+                  <span>
+                    정기 배송 시 매달{"\u00A0"}
+                    {(
+                      (product.baseDiscountedPrice - product.calculatedRegularPrice) *
+                      quantity
+                    ).toLocaleString()}
+                    원 할인!
+                  </span>
+                </div>
+              )}
           </div>
         </div>
       ) : (

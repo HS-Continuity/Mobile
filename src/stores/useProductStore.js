55d94150fabeb5c6f3409dee168d2397eb8f6e08
정승수 <victory-soo@naver.com>
2024-07-23 15:46:16 +0900

    KAN-486 refactor: 안쓰는 store 제거

--- src/stores/useProductStore.js
+++
@@ -1,7 +0,0 @@
-// import { create } from "zustand";
-
-// export const useProductStore = create(set => ({
-//   products: [],
-//   setProducts: products => set({ products }),
-//   addProducts: newProducts => set(state => ({ products: [...state.products, ...newProducts] })),
-// }));
--- src/stores/useShopStore.js
+++
@@ -1,6 +0,0 @@
-// import { create } from "zustand";
-
-// export const useShopStore = create(set => ({
-//   selectedCategory: [],
-//   setSelectedCategory: category => set({ selectedCategory: category }),
-// }));

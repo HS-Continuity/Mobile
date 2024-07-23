40536aae97a6bc117bf9236ea63d3221261fccb1
정승수 <victory-soo@naver.com>
2024-07-23 09:16:59 +0900

    KAN-490 refactor: 검색 페이지 폴더구조 변경

---
+++ src/pages/search/SearchResult.jsx
@@ -0,0 +1,72 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSearchProductsQuery } from "../../apis";
+import ProductList from "../../components/Product/ProductList";
+import { useEffect, useRef, useState } from "react";
+import { useSearchStore } from "../../stores/useSearchStore";
+import { FiSearch, FiX } from "react-icons/fi";
+
+const SearchResult = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const initialKeyword = searchParams.get("keyword") || "";
+  const [searchQuery, setSearchQuery] = useState(initialKeyword);
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
+
+  const { addSearch } = useSearchStore();
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    setSearchQuery(initialKeyword);
+  }, [initialKeyword]);
+
+  const handleSearchSubmit = e => {
+    e.preventDefault();
+    if (searchQuery.trim()) {
+      performSearch(searchQuery);
+    }
+  };
+
+  const performSearch = searchTerm => {
+    addSearch(searchTerm.trim());
+    const searchPath = `/search-result?keyword=${encodeURIComponent(searchTerm.trim())}`;
+    navigate(searchPath);
+  };
+
+  return (
+    <div>
+      <div className='main-container h-full w-full overflow-y-auto bg-white'>
+        <div className='p-4'>
+          <form onSubmit={handleSearchSubmit} className='relative mb-4'>
+            <input
+              ref={inputRef}
+              type='text'
+              placeholder='검색어를 입력해주세요.'
+              className='input input-bordered h-[35px] w-full rounded-full pl-10'
+              value={searchQuery}
+              onChange={e => setSearchQuery(e.target.value)}
+            />
+            <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-400' />
+            {searchQuery && (
+              <FiX
+                className='absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-400'
+                onClick={() => navigate("/search")}
+              />
+            )}
+          </form>
+          <h2 className='mb-4 text-2xl font-bold'>{initialKeyword} 검색 결과</h2>
+          <ProductList
+            useQueryHook={useSearchProductsQuery}
+            additionalProps={initialKeyword}
+            gridCols={2}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResult;

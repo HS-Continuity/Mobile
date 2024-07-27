ed1e029d24add97ef38f9dec8457396b915a456c
정승수 <victory-soo@naver.com>
2024-07-27 21:02:58 +0900

    KAN-341 feat: 멤버 로그인 API 연동

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,52 +1,147 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import kakaoLogo from "../../assets/images/kakao_icon.png";
+import googleLogo from "../../assets/images/google_icon.png";
+import logo from "../../assets/images/logo.png";
+import { useMutation } from "@tanstack/react-query";
+import { handleSocialLogin, postLogin } from "../../apis";
+import toast, { Toaster } from "react-hot-toast";
+import useAuthStore from "../../stores/useAuthStore";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = React.useState(false);
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginData, setLoginData] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({ username: "", password: "" });
+  const [touched, setTouched] = useState({ username: false, password: false });
+
+  const login = useAuthStore(state => state.login);
+
   const socialLogins = [
     { name: "kakao", icon: kakaoLogo },
-    // { name: 'facebook', icon: facebookLogo },
+    { name: "google", icon: googleLogo },
     // { name: 'apple', icon: appleLogo },
     // { name: 'naver', icon: naverLogo },
   ];
 
+  const loginMutation = useMutation({
+    mutationFn: login,
+    onSuccess: data => {
+      if (data) {
+        // login 함수가 true를 반환했을 때만 성공으로 처리
+        toast.success("로그인 성공!");
+        navigate(-1);
+      } else {
+        toast.error("로그인에 실패했습니다.");
+      }
+    },
+    onError: error => {
+      console.error("Login error:", error);
+      if (error.response && error.response.status === 401) {
+        toast.error("아이디와 비밀번호가 일치하지 않습니다.");
+      } else {
+        toast.error("로그인 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+      }
+    },
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setLoginData(prev => ({ ...prev, [name]: value }));
+    validateField(name, value);
+  };
+
+  const handleBlur = e => {
+    const { name } = e.target;
+    setTouched(prev => ({ ...prev, [name]: true }));
+    validateField(name, loginData[name]);
+  };
+
+  const validateField = (fieldName, value) => {
+    let errorMessage = "";
+    if (value.trim() === "") {
+      errorMessage = `${fieldName === "username" ? "아이디" : "비밀번호"}를 입력해주세요.`;
+    }
+    setErrors(prev => ({ ...prev, [fieldName]: errorMessage }));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (loginData.username && loginData.password) {
+      try {
+        await loginMutation.mutateAsync(loginData);
+      } catch (error) {
+        // 오류는 mutation의 onError에서 처리됩니다.
+      }
+    } else {
+      toast.error("아이디와 비밀번호를 모두 입력해주세요.");
+    }
+  };
+
+  const isFormValid = () => {
+    return Object.keys(loginData).every(key => loginData[key]);
+  };
+
   return (
     <div className='flex h-full flex-col bg-white p-4'>
       <div className='flex flex-grow flex-col justify-center'>
-        <div className='space-y-4 p-4'>
+        <form onSubmit={handleSubmit} className='space-y-4 p-4'>
+          <img src={logo} alt='logo' className='mx-auto -mb-2 w-14' />
+          <p className='text-center text-4xl font-extrabold text-[#00835F]'>연이음</p>
+          {/* 아이디 */}
           <div className='relative'>
             <input
               type='text'
               placeholder='아이디'
+              name='username'
+              value={loginData.username}
+              onChange={handleChange}
+              onBlur={handleBlur}
               className='input input-bordered h-14 w-full rounded-b-none focus:border-black focus:outline-none focus:ring-0'
             />
+
+            {/* 비밀번호 */}
             <input
               type={showPassword ? "text" : "password"}
               placeholder='비밀번호'
+              name='password'
+              value={loginData.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
               className='input input-bordered h-14 w-full rounded-t-none focus:border-black focus:outline-none focus:ring-0'
             />
+
             <button
+              type='button'
               onClick={() => setShowPassword(!showPassword)}
               className='absolute bottom-4 right-4 text-gray-400'>
               {showPassword ? <FaEyeSlash size={23} /> : <FaEye size={23} />}
             </button>
           </div>
-
-          <div className='flex items-center'>
-            <input type='checkbox' className='checkbox-success checkbox checkbox-sm mr-2' />
-            <span className='text-sm text-gray-600'>로그인 상태 유지</span>
+          <div>
+            {touched.username && errors.username && (
+              <p className='text-xs text-red-500'>{errors.username}</p>
+            )}
+            {touched.password && errors.password && (
+              <p className='text-xs text-red-500'>{errors.password}</p>
+            )}
           </div>
 
-          <button className='btn btn-success w-full text-white'>로그인</button>
-        </div>
+          {/* <div className='flex items-center'>
+            <input type='checkbox' className='checkbox-success checkbox checkbox-sm mr-2' />
+            <span className='text-sm text-gray-600'>로그인 상태 유지</span>
+          </div> */}
 
-        <div className='mt-4 flex justify-center space-x-4 text-sm text-gray-500'>
-          <Link to='/find-id'>아이디 찾기</Link>
-          <span>|</span>
-          <Link to='/reset-password'>비밀번호 재설정</Link>
-          <span>|</span>
+          <button
+            type='submit'
+            className='btn w-full bg-green-shine text-white hover:bg-green-shine'
+            disabled={loginMutation.isLoading || !isFormValid()}>
+            {loginMutation.isLoading ? "로그인 중..." : "로그인"}
+          </button>
+        </form>
+
+        <div className='mt-4 flex justify-center space-x-4 text-sm text-[#00835F]'>
           <Link to='/signup'>회원가입</Link>
         </div>
 
@@ -55,7 +150,10 @@ const Login = () => {
           <h2 className='mb-4 text-center text-gray-500'>간편 로그인</h2>
           <div className='flex justify-center space-x-4'>
             {socialLogins.map(social => (
-              <button key={social.name} className='h-12 w-12 overflow-hidden rounded-full'>
+              <button
+                key={social.name}
+                className='h-12 w-12 overflow-hidden rounded-full'
+                onClick={() => handleSocialLogin(social.name)}>
                 <img
                   src={social.icon}
                   alt={`${social.name} login`}
@@ -66,6 +164,7 @@ const Login = () => {
           </div>
         </div>
       </div>
+      <Toaster position='bottom-center' />
     </div>
   );
 };
